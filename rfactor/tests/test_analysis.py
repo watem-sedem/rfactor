20f9c5f9a8315691ac88b12ac612e566b8f80646
Sacha <sacha@fluves.com>
2021-03-18 17:17:36 +0100

    unit tests for data_processing

---
+++ rfactor/tests/test_analysis.py
@@ -0,0 +1,48 @@
+import pytest
+import numpy as np
+from pathlib import Path
+from rfactor.src.flanders.data_processing import ErosivityData
+
+
+@pytest.mark.parametrize(
+    "exclude_stations,rfactor",[(["KMI_6447","KMI_FS3"],1263.2720860927154),
+                                (["KMI_6447","KMI_FS3","KMI_6455","KMI_6459","KMI_6472","KMI_6494","KMI_6484"],1235.2283858267717)]
+
+)
+def test_flanders(exclude_stations,rfactor):
+
+    fmap= Path(__file__).parent / "data"
+    fmap_rainfall=  fmap / "test_rainfalldata"
+    fmap_erosivity = fmap / "test_erosivitydata"
+    txt_files=  fmap / "datafiles_completeness.csv"
+
+    data = ErosivityData(fmap_rainfall,fmap_erosivity)
+    df_files = data.build_data_set(txt_files)
+    data.load_data(df_files)
+    stations = [station for station in data.stations if station not in exclude_stations]
+    df_R=data.load_R(stations)
+
+    np.testing.assert_allclose(np.mean(df_R["value"]),rfactor,atol=1e-2)
+
+@pytest.mark.parametrize(
+    "timeseries,rfactor", [(range(1898,2003,1),958.1058095238096),
+              (range(2003,2020,1),1313.013125),
+              (range(1898,2020,1),1005.0357024793389),
+              (range(1996,2020,1),1260.6330434782608),
+              (range(1990,2020,1),1244.8975862068964),
+              (range(1990,2001,1),1094.6436363636365),
+              (range(2000, 2020,1),1302.7794736842104)],
+)
+def test_ukkel(timeseries,rfactor):
+
+    fmap= Path(__file__).parent / "data"
+    fmap_rainfall=  fmap / "test_rainfalldata"
+    fmap_erosivity = fmap / "test_erosivitydata"
+    txt_files=  fmap / "datafiles_completeness.csv"
+
+    data = ErosivityData(fmap_rainfall,fmap_erosivity)
+    df_files = data.build_data_set(txt_files)
+    data.load_data(df_files)
+    df_R=data.load_R(["KMI_6447","KMI_FS3"], timeseries)
+
+    np.testing.assert_allclose(np.mean(df_R["value"]),rfactor,atol=1e-2)
