48918a15d2b37bc51d300f4f4929484804f38d96
Sacha <sacha@fluves.com>
2021-03-24 17:01:54 +0100

    update tests with test of matlab script

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,18 +3,20 @@ from pathlib import Path
 
 folder_data = Path("tests/data")
 
+# references to input data
+txt_files = (folder_data / "files.csv").absolute()
+fmap_rainfall = (folder_data / "test_rainfalldata").absolute()
+fmap_erosivity = (folder_data / "test_erosivitydata").absolute()
+
+
 def create_data_instance_with_test_data():
 
-    # references to input data
-    txt_files = folder_data / "files.csv"
-    fmap_rainfall = folder_data / "test_rainfalldata"
-    fmap_erosivity = folder_data / "test_erosivitydata"
-
     # create erosvity data instance and load data
-    erosivitydata = ErosivityData(fmap_rainfall,fmap_erosivity)
+    erosivitydata = ErosivityData(fmap_rainfall, fmap_erosivity)
     df_files = erosivitydata.build_data_set(txt_files)
     erosivitydata.load_data(df_files)
 
     return erosivitydata
+
 
 erosivitydata = create_data_instance_with_test_data()
