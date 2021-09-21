232927737455574e8ae63487e62b3b59de14b308
Stijn Van Hoey <stijn@fluves.com>
2021-09-21 18:31:04 +0200

    Adjust paths for cmd runs
    Former-commit-id: 3f8eee13a7a257d88fa481f1488860fa3a5239ed

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,9 @@ def rain_benchmark_closure():
 
     def rain_benchmark_year(station, year):
         """Get benchmark data for given year and station"""
-        rain = load_rain_file(Path(f"./data/test_rainfalldata/{station}_{year}.txt"))
+        rain = load_rain_file(
+            Path(f"./tests/data/test_rainfalldata/{station}_{year}.txt")
+        )
         return rain
 
     return rain_benchmark_year
@@ -22,14 +24,14 @@ def rain_benchmark_closure():
 @pytest.fixture()
 def rain_benchmark_data():
     """Rain data used for benchmark reference case"""
-    return load_rain_folder(Path("./data/test_rainfalldata"))
+    return load_rain_folder(Path("./tests/data/test_rainfalldata"))
 
 
 @pytest.fixture()
 def erosivity_benchmark_data():
     """Erosivity output used for benchmark reference case"""
     erosivity = pd.read_csv(
-        "./data/test_erosivitydata/testdata_maximum_intensity.csv",
+        "./tests/data/test_erosivitydata/testdata_maximum_intensity.csv",
         index_col=0,
         parse_dates=[0, 1],
     )
