89c12c1af05baab38b18b1021f47175860521b12
Stijn Van Hoey <stijn@fluves.com>
2021-09-21 22:54:02 +0200

    Remove non-used fixtures
    Former-commit-id: 7acc0af9c27edb4c7a3a8865c950d936a896a5f7

--- tests/conftest.py
+++ tests/conftest.py
@@ -134,39 +134,3 @@ def station_metadata():
     return pd.DataFrame(
         {"station": ["station_0", "station_1"], "x": [4.0, 4.1], "y": [51.2, 51.1]}
     )
-
-
-# folder_data = Path("tests/data")
-# folder_example = Path("docs/example_data/")
-#
-# # references to input data
-# txt_files = (folder_data / "files_tests.csv").absolute()
-# fmap_rainfall = (folder_data / "test_rainfalldata").absolute()
-# fmap_erosivity = (folder_data / "test_erosivitydata").absolute()
-#
-# fmap_rainfall_one_file = (folder_example / "rainfall").absolute()
-# fmap_erosivity_one_file = (folder_example / "erosivity").absolute()
-#
-# def create_data_instance_with_test_data():
-#     """
-#     Compile database with test rainfall and erosivity data
-#
-#     The aim of this function is to create a data instance with the given test
-#     data. This data instance will be used in the test functions to compute
-#     EI30 and R-values. Make use of files.csv file to define which specific
-#     input data files are considered to compute EI30 and R-factor values.
-#
-#     Returns
-#     -------
-#     erosivitydata: rfactor.process.ErosivityData
-#         See :class:`rfactor.process.ErosivityData`
-#     """
-#     # create erosvity data instance and load data
-#     erosivitydata = ErosivityData(fmap_rainfall, fmap_erosivity)
-#     df_files = erosivitydata.build_data_set(txt_files)
-#     erosivitydata.load_data(df_files)
-#
-#     return erosivitydata
-#
-#
-# erosivitydata = create_data_instance_with_test_data()
