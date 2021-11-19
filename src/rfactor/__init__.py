dd7a786369cf9a697fd1b06983646cd1cee372e2
Stijn Van Hoey <stijn@fluves.com>
2021-11-19 18:38:21 +0100

    Fix isort

--- src/rfactor/__init__.py
+++ src/rfactor/__init__.py
@@ -1,16 +1,14 @@
 import sys
 
-from .rfactor import (
-    compute_erosivity,
-    maximum_intensity,
-    maximum_intensity_matlab_clone,
-)
+from rfactor import compute_erosivity, maximum_intensity, maximum_intensity_matlab_clone
 
 if sys.version_info[:2] >= (3, 8):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+    from importlib.metadata import PackageNotFoundError  # pragma: no cover
+    from importlib.metadata import version
 else:
-    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+    from importlib_metadata import PackageNotFoundError  # pragma: no cover
+    from importlib_metadata import version
 
 try:
     # Change here if project is renamed and does not equal the package name
