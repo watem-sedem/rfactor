18465358a4ae1481f8b14a4871602f4692b4cb8e
Sachagobeyn <Sachagobeyn@users.noreply.github.com>
2021-11-25 15:47:13 +0000

    deploy: b1aada0d780bc2dcf56482289f6e0b96941ce5b9
    Former-commit-id: 08f1741e8d2c38734739daf5301c114481191bfa

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0.post1.dev1+g2dd4d2ac',
+    VERSION: '0.1.0.post1.dev2+g8d8102bd',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
