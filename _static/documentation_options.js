c1febcae68bd01bf8da7a15f98b1ea5c0c424a85
stijnvanhoey <stijnvanhoey@users.noreply.github.com>
2021-12-03 14:07:02 +0000

    deploy: d96573bcbd4788a25150e9ac8dd35d354e2556bd [formerly 29fdea49e94397a85792b2bc9847ed4d59a150ac]
    Former-commit-id: 64c445bf8e83455e0185edac01ddf459d64a2447

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0.post1.dev2+g8d8102bd',
+    VERSION: '0.1.0.post1.dev4+gc885929d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
