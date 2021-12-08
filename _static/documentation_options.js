069ae43cdbd53e91126df2f4adceda4db25810ca
Stijn Van Hoey <stijn@fluves.com>
2021-12-08 16:01:49 +0000

    commit e9868390d6e1c0ca97c097787cc16d1acd2c354f [formerly b18bbee2cf21aa84d01f15862bb6b8448f402ca5] Merge: f71b606 223fa37 Author: Stijn Van Hoey <stijn@fluves.com> Date:   Wed Dec 8 16:55:01 2021 +0100
        Merge pull request #50 from cn-ws/clean-data-files
    
        Clean data files
    
    
    Former-commit-id: af7117baa0f376353cec281ec7c338645dd5ad69

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post1.dev328+g1a5c8c5.d20211208',
+    VERSION: '0.0.post1.dev344+g9f66885.d20211208',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
