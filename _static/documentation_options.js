97c81ecd44b053fc254b751090a375b18281296d
SachaGobeyn <sacha@fluves.com>
2021-12-09 10:19:36 +0000

    commit 7b9b2cfed119c3e9877919d9cd7295d0cdd093a7 Author: SachaGobeyn <sacha@fluves.com> Date:   Thu Dec 9 11:12:54 2021 +0100
        Update changelog for release  0.1.1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post1.dev348+g8f28442.d20211209',
+    VERSION: '0.0.post1.dev349+g7b9b2cf.d20211209',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
