14c1c09795879499b380110f707b7c894c3f6d9b
Sachagobeyn <Sachagobeyn@users.noreply.github.com>
2021-12-16 15:20:03 +0000

    deploy: d6db1665a26bd6148583acbc42ec556d931cc2a3

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post1.dev350+g48c064e.d20211209',
+    VERSION: '0.1.1.post1.dev3+gd6db166',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
