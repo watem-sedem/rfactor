842bba926647d21606b50ea228d7e96bd6212d66
Stijn Van Hoey <stijn@fluves.com>
2021-12-18 09:52:18 +0000

    commit 4502a3ec0c76a2598c4370b1fb315f5c49445acb Author: Stijn Van Hoey <stijn@fluves.com> Date:   Sat Dec 18 10:45:15 2021 +0100
        add pip install instructions

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.post1.dev7+g1c5ef6c',
+    VERSION: '0.0.post1.dev356+g4502a3e.d20211218',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
