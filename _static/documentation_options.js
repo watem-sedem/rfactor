9c443cb54bbcf7166960cf1113f40e5b26b6708b
Sacha <sacha@fluves.com>
2022-11-02 13:54:00 +0000

    commit 95604561d5c1661e53dc68ee1db8a13fe575eb8a Merge: 40c0e10 406b46f Author: Sacha <sacha@fluves.com> Date:   Wed Nov 2 14:46:34 2022 +0100
        Merge pull request #56 from cn-ws/fix_earlyfail_folder
    
        Fix earlyfail folder

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post1.dev373+g40c0e10.d20220802',
+    VERSION: '0.0.post1.dev382+g9560456.d20221102',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
