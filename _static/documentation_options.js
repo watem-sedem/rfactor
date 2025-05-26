9d14c0334ad82ba0821b57f9768118f4b2065211
Sachagobeyn <Sachagobeyn@users.noreply.github.com>
2025-05-26 18:59:06 +0000

    deploy: 068bbecadfb9c628cce863aa113fc7c4f11049c3

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.3.post1.dev38+g1080150',
+    VERSION: '0.1.3.post1.dev37+g068bbec',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
